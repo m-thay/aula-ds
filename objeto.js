84b887d866448407226608239fa69bf17316ff74
maria.thayssa <maria.thayssa@aluno.edu.pi.gov.br>
2026-06-16 19:18:28 +0000

    vai da certo

---
+++ objeto.js
@@ -0,0 +1,12 @@
+const animal ={
+    nome: "Zeus",
+    idade: 8,
+    peso:"ideterminado",
+    raca:"rottweiller"
+}
+
+console.log(animal.nome);
+animal.idade = 8;
+console.log(animal.idade);
+console.log(animal.peso);
+console.log(animal.raca);
