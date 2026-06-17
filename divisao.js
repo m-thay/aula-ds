ad62c793024e402393d6643ab61238b9908c5ac2
maria.thayssa <maria.thayssa@aluno.edu.pi.gov.br>
2026-06-17 13:53:58 +0000

    divisao

---
+++ divisao.js
@@ -0,0 +1,8 @@
+function divisao(a, b) {
+    if (b === 0) {
+        return "Erro: divisão por zero";
+    }
+    return a / b;
+}
+
+console.log(divisao(10, 2)); // 5
---
+++ objfuncao.js
@@ -0,0 +1,17 @@
+const calculadora={
+    numero:10,
+    numero2:5,
+    multiplicar( ){
+        return this.numero1*this.numero2}
+    dividir
+    (a,b){
+        return a/b;
+    }
+    
+    
+    
+    
+    
+    
+    }
+console.log(calculadors.multiplicar());
